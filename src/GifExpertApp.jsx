4a84f9e28ab2e36d9001d5e5c1457f91a5c4768f
Kevin Carrillo <kcarrillo@joonik.com>
2023-09-15 19:19:38 -0500

    useeffect

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,32 +1,22 @@
-import { useState } from "react"
+import { useState } from "react";
 import { AddCategory } from "./components/AddCategory";
+import { GiftGrid } from "./components/GiftGrid";
 
 export const GifExpertApp = () => {
-    const [categories, setCategories] = useState(['One Punch']);
-    const onAddCategory = ( newCategory ) => {
-        // categories.push( newCategory );
-        setCategories([newCategory, ...categories] );
-        // setcategories(cat => [...cat, 'Valorant']);
-
-    }
-    return (
-        <>
-            {/* Titulo */}
-            <h1>GifExpertApp</h1>
-            {/* Input */}
-            <AddCategory 
-                // setCategories = { setCategories }
-                onNewCategory={ event => onAddCategory(event) }
-            />
-            {/* Listado de gif */}
-            <ol>
-                {categories.map(category => {
-                    return <li key={category}>{category}</li>
-                })}
-            </ol>
-            {/* Gif Item */}
-        </>
-    )
-}
-
-
+  const [categories, setCategories] = useState(["One Punch"]);
+  const onAddCategory = (newCategory) => {
+    if (categories.includes(newCategory)) return;
+    // categories.push( newCategory );
+    setCategories([newCategory, ...categories]);
+    // setcategories(cat => [...cat, 'Valorant']);
+  };
+  return (
+    <>
+      <h1>GifExpertApp</h1>
+      <AddCategory onNewCategory={(event) => onAddCategory(event)} />
+      {categories.map((category) => (
+        <GiftGrid key={category} category={category} />
+      ))}
+    </>
+  );
+};
--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,31 +1,29 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export const AddCategory = ({ onNewCategory }) => {
+  const [inputValue, setInputvalue] = useState("One punch");
 
-    const [inputValue, setInputvalue] = useState('One punch');
+  const onInputChange = ({ target }) => {
+    setInputvalue(target.value);
+  };
 
-    const onInputChange = ({ target }) => {
-        setInputvalue( target.value );
-    }
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (inputValue.trim() <= 1) return;
 
-    const onSubmit = ( event ) => {
-        event.preventDefault();
-        if (inputValue.trim() <= 1) return;
+    // setCategories( categories => [ inputValue, ...categories ]);
+    onNewCategory(inputValue.trim());
+    setInputvalue("");
+  };
 
-
-        // setCategories( categories => [ inputValue, ...categories ]);
-        onNewCategory( inputValue.trim() );
-        setInputvalue('');
-    }
-
-    return (
-        <form onSubmit={ onSubmit } action="">
-            <input 
-                type="text"
-                placeholder="Buscar gifs"
-                value={ inputValue }
-                onChange={ onInputChange }
-            />
-        </form>
-    )
-}
+  return (
+    <form onSubmit={onSubmit} action="">
+      <input
+        type="text"
+        placeholder="Buscar gifs"
+        value={inputValue}
+        onChange={onInputChange}
+      />
+    </form>
+  );
+};
