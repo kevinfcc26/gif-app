a3957c1bd3961f0224d17c8c4fa163f9a2b346ff
Kevin Carrillo <kcarrillo@joonik.com>
2023-09-19 20:19:49 -0500

    add index to export all as a package

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import { AddCategory } from "./components/AddCategory";
-import { GiftGrid } from "./components/GiftGrid";
+import { AddCategory, GiftGrid } from "./components";
 
 export const GifExpertApp = () => {
   const [categories, setCategories] = useState(["One Punch"]);
